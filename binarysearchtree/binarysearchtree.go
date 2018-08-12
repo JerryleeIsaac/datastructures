bb0e74c457bcb6a7e4fc8af36c236eb8a5cf8d79
Jerrylee Isaac Geronimo <jerryleegeronimo@yahoo.com>
2018-08-12 10:58:16 +0800

    Modified binary search tree algorithms to use  a Node struct instead of an interface of a binary search tree

--- binarysearchtree/binarysearchtree.go
+++ binarysearchtree/binarysearchtree.go
@@ -1,16 +1,11 @@
 package binarysearchtree
 
-// BinarySearchTree is an interface containing generic methods of a binary search tree
-type BinarySearchTree interface {
+// Node of a binary search tree
+type Node struct {
 	Element
-	LeftChild() BinarySearchTree
-	RightChild() BinarySearchTree
-	Parent() BinarySearchTree
-	SetLeftChild(BinarySearchTree)
-	SetRightChild(BinarySearchTree)
-	SetParent(BinarySearchTree)
-	SetElement(e Element)
-	GetElement() Element
+	LeftChild  *Node
+	RightChild *Node
+	Parent     *Node
 }
 
 // Element is an element stored in a node of a binary search tree
@@ -20,7 +15,7 @@ type Element interface {
 }
 
 // Insert adds a new node to a binary search tree
-func Insert(root BinarySearchTree, newNode BinarySearchTree) BinarySearchTree {
+func Insert(root *Node, newNode *Node) *Node {
 	// Create new node if root is nil
 	//
 	if root == nil {
@@ -30,17 +25,17 @@ func Insert(root BinarySearchTree, newNode BinarySearchTree) BinarySearchTree {
 	// Insert on left or right subtree depending on the value of the new node
 	//
 	if root.Value() >= newNode.Value() {
-		root.SetLeftChild(Insert(root.LeftChild(), newNode))
-		root.LeftChild().SetParent(newNode)
+		root.LeftChild = Insert(root.LeftChild, newNode)
+		root.LeftChild.Parent = root
 	} else {
-		root.SetRightChild(Insert(root.RightChild(), newNode))
-		root.RightChild().SetParent(newNode)
+		root.RightChild = Insert(root.RightChild, newNode)
+		root.RightChild.Parent = root
 	}
 	return root
 }
 
 // Find looks for the node containing the element in the binary search tree
-func Find(root BinarySearchTree, node BinarySearchTree) (BinarySearchTree, bool) {
+func Find(root *Node, node *Node) (*Node, bool) {
 	if root == nil {
 		return nil, false
 	}
@@ -49,16 +44,16 @@ func Find(root BinarySearchTree, node BinarySearchTree) (BinarySearchTree, bool)
 	// node we are looking for
 	//
 	if root.Value() > node.Value() {
-		return Find(root.LeftChild(), node)
+		return Find(root.LeftChild, node)
 	}
 	if root.Value() < node.Value() {
-		return Find(root.RightChild(), node)
+		return Find(root.RightChild, node)
 	}
 	return root, true
 }
 
 // Remove deletes an element on a binary search tree
-func Remove(root BinarySearchTree, node BinarySearchTree) (BinarySearchTree, bool) {
+func Remove(root *Node, node *Node) (*Node, bool) {
 	if root == nil {
 		return nil, false
 	}
@@ -66,89 +61,99 @@ func Remove(root BinarySearchTree, node BinarySearchTree) (BinarySearchTree, boo
 	// Recursively look for the node to be deleted by following the pattern for find
 	//
 	if root.Value() > node.Value() {
-		return Remove(root.LeftChild(), node)
+		return Remove(root.LeftChild, node)
 	}
 	if root.Value() < node.Value() {
-		return Remove(root.RightChild(), node)
+		return Remove(root.RightChild, node)
 	}
 
-	var temp BinarySearchTree
-	if root.LeftChild() != nil && root.LeftChild().Value() == node.Value() {
+	var temp *Node
+	if root.LeftChild != nil && root.LeftChild.Value() == node.Value() {
 		// Case A: Node to be deleted is on the left child
 		//
-		temp = root.LeftChild()
-		if temp.LeftChild() == nil && temp.RightChild() == nil {
+		temp = root.LeftChild
+		if temp.LeftChild == nil && temp.RightChild == nil {
 			// Case A1: Node to be deleted has no children
 			//
-			root.SetLeftChild(nil)
-		} else if temp.LeftChild() != nil && temp.RightChild() != nil {
+			root.LeftChild = nil
+		} else if temp.LeftChild != nil && temp.RightChild != nil {
 			// Case A2: Node to be deleted has two children
 			//
-			del := temp.RightChild()
-			for del.RightChild() != nil {
-				del = del.RightChild()
+			del := temp.RightChild
+			for del.RightChild != nil {
+				del = del.RightChild
 			}
-			del.Parent().SetRightChild(nil)
-			temp.SetElement(del.GetElement())
+			del.Parent.RightChild = nil
+			temp.Element = del.Element
 		} else {
 			// Case A3: Node to be deleted has only one children
 			//
-			if temp.LeftChild() != nil {
-				root.SetLeftChild(temp.LeftChild())
-				temp.LeftChild().SetParent(root)
-			} else if temp.RightChild() != nil {
-				root.SetLeftChild(temp.RightChild())
-				temp.RightChild().SetParent(root)
+			if temp.LeftChild != nil {
+				root.LeftChild = temp.LeftChild
+				temp.LeftChild.Parent = root
+			} else if temp.RightChild != nil {
+				root.LeftChild = temp.RightChild
+				temp.RightChild.Parent = root
 			}
 		}
-	} else if root.RightChild() != nil && root.RightChild().Value() == node.Value() {
+	} else if root.RightChild != nil && root.RightChild.Value() == node.Value() {
 		// Case B: Node to be deleted is on the right child
 		//
-		temp = root.RightChild()
-		if temp.LeftChild() == nil && temp.RightChild() == nil {
+		temp = root.RightChild
+		if temp.LeftChild == nil && temp.RightChild == nil {
 			// Case B1: Node to be deleted has no children
 			//
-			root.SetRightChild(nil)
-		} else if temp.LeftChild() != nil && temp.RightChild() != nil {
+			root.RightChild = nil
+		} else if temp.LeftChild != nil && temp.RightChild != nil {
 			// Case B2: Node to be deleted has two children
 			//
-			del := temp.LeftChild()
-			for del.LeftChild() != nil {
-				del = del.LeftChild()
+			del := temp.LeftChild
+			for del.LeftChild != nil {
+				del = del.LeftChild
 			}
-			del.Parent().SetLeftChild(nil)
-			temp.SetElement(del.GetElement())
+			del.Parent.LeftChild = nil
+			temp.Element = del.Element
 		} else {
 			// Case B3: Node to be deleted has only one children
 			//
-			if temp.LeftChild() != nil {
-				root.SetRightChild(temp.LeftChild())
-				temp.LeftChild().SetParent(root)
-			} else if temp.RightChild() != nil {
-				root.SetRightChild(temp.RightChild())
-				temp.RightChild().SetParent(root)
+			if temp.LeftChild != nil {
+				root.RightChild = temp.LeftChild
+				temp.LeftChild.Parent = root
+			} else if temp.RightChild != nil {
+				root.RightChild = temp.RightChild
+				temp.RightChild.Parent = root
 			}
 		}
 	}
 	return temp, true
 }
 
-func leftRotate(b BinarySearchTree) BinarySearchTree {
-	x := b.RightChild()
-	z := x.LeftChild()
+// LeftRotate rotates a node in a binary search tree
+//
+func LeftRotate(b *Node) *Node {
+	x := b.RightChild
+	z := x.LeftChild
 
-	x.SetLeftChild(b)
-	b.SetRightChild(z)
+	x.LeftChild = b
+	b.Parent = x
+
+	b.RightChild = z
+	z.Parent = b
 
 	return x
 }
 
-func rightRotate(b BinarySearchTree) BinarySearchTree {
-	x := b.LeftChild()
-	z := x.RightChild()
+// RightRotate rotates a node in a binary search tree
+//
+func RightRotate(b *Node) *Node {
+	x := b.LeftChild
+	z := x.RightChild
 
-	x.SetRightChild(b)
-	b.SetLeftChild(z)
+	x.RightChild = b
+	b.Parent = x
+
+	b.LeftChild = z
+	z.Parent = b
 
 	return x
 }
