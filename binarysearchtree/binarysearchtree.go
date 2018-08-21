98301910730649cb603da38b399603b42ef97ec9
Jerrylee Isaac Geronimo <jerryleegeronimo@yahoo.com>
2018-08-21 15:08:56 +0800

    Done implementing red black trees

--- binarysearchtree/binarysearchtree.go
+++ binarysearchtree/binarysearchtree.go
@@ -1,6 +1,9 @@
 package binarysearchtree
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // Node of a binary search tree
 type Node struct {
@@ -8,29 +11,31 @@ type Node struct {
 	LeftChild  *Node
 	RightChild *Node
 	Parent     *Node
+	IsNil      bool
 
 	// Red Black Tree node properties
 	color int
 }
 
-func (n Node) String() string {
-	s := fmt.Sprint(n.Value(), " ")
-
-	if n.LeftChild != nil {
-		s += fmt.Sprint(n.LeftChild.Value(), " ")
+func (n *Node) String() string {
+	s := ""
+	if n.IsNil {
+		s = "<nil> <nil> <nil> "
 	} else {
-		s += "<nil> "
+		s = fmt.Sprint(n.Value(), " ")
+		if !n.LeftChild.IsNil {
+			s += fmt.Sprint(n.LeftChild.Value(), " ")
+		} else {
+			s += "<nil> "
+		}
+		if !n.RightChild.IsNil {
+			s += fmt.Sprint(n.RightChild.Value(), " ")
+		} else {
+			s += "<nil> "
+		}
 	}
-	if n.RightChild != nil {
-		s += fmt.Sprint(n.RightChild.Value(), " ")
-	} else {
-		s += "<nil> "
-	}
-
-	if n.Parent != nil {
+	if !n.Parent.IsNil {
 		s += fmt.Sprint(n.Parent.Value(), " ")
-	} else {
-		s += "<nil> "
 	}
 
 	switch n.color {
@@ -38,6 +43,8 @@ func (n Node) String() string {
 		s += "Black "
 	case Red:
 		s += "Red "
+	case DoubleBlack:
+		s += "Double black"
 	}
 
 	return s
@@ -45,7 +52,21 @@ func (n Node) String() string {
 
 // NewNode creates a node object with default values
 func NewNode(e Element) *Node {
-	return &Node{Element: e, color: Red}
+	newNode := NewNilNode()
+	newNode.Element = e
+	newNode.color = Red
+	newNode.IsNil = false
+	return newNode
+}
+
+// NewNilNode creates a new nil node
+func NewNilNode() *Node {
+	newNode := &Node{IsNil: true}
+	newNode.LeftChild = &Node{IsNil: true, Parent: newNode}
+	newNode.RightChild = &Node{IsNil: true, Parent: newNode}
+	newNode.Parent = &Node{IsNil: true}
+
+	return newNode
 }
 
 // Element is an element stored in a node of a binary search tree
@@ -59,7 +80,7 @@ func Insert(root *Node, e Element) (*Node, *Node) {
 	var newNode *Node
 	// Create new node if root is nil
 	//
-	if root == nil {
+	if root.IsNil {
 		newNode = NewNode(e)
 		return newNode, newNode
 	}
@@ -77,78 +98,105 @@ func Insert(root *Node, e Element) (*Node, *Node) {
 }
 
 // Find looks for the node containing the element in the binary search tree
-func Find(root *Node, node *Node) (*Node, bool) {
-	if root == nil {
-		return nil, false
+func Find(root *Node, e Element) (*Node, bool) {
+	if root.IsNil {
+		return root, false
 	}
 
 	// Look for the element on the left or right subtree depending on the value of the
 	// node we are looking for
 	//
-	if root.Value() > node.Value() {
-		return Find(root.LeftChild, node)
+	if root.Value() > e.Value() {
+		return Find(root.LeftChild, e)
 	}
-	if root.Value() < node.Value() {
-		return Find(root.RightChild, node)
+	if root.Value() < e.Value() {
+		return Find(root.RightChild, e)
 	}
 	return root, true
 }
 
 // Remove deletes an element on a binary search tree
-func Remove(root *Node, e Element) *Node {
-	if root == nil {
-		return nil
+func Remove(root *Node, e Element) (*Node, *Node) {
+	if root.IsNil {
+		return root, root
 	}
+
+	var newNode *Node
 
 	// Recursively look for the node to be deleted by following the pattern for find
 	//
 	if root.Value() > e.Value() {
-		root.LeftChild = Remove(root.LeftChild, e)
+		root.LeftChild, newNode = Remove(root.LeftChild, e)
+		root.LeftChild.Parent = root
 	} else if root.Value() < e.Value() {
-		root.RightChild = Remove(root.RightChild, e)
+		root.RightChild, newNode = Remove(root.RightChild, e)
+		root.RightChild.Parent = root
 	} else {
 
 		// Case with no child or one child
 		//
-		if root.LeftChild == nil {
-			return root.RightChild
-		} else if root.RightChild == nil {
-			return root.LeftChild
+		if !root.LeftChild.IsNil && !root.RightChild.IsNil {
+			// Case with 2 children
+			//
+			if time.Now().Unix()%2 == 0 {
+				del := root.LeftChild
+				for !del.RightChild.IsNil {
+					del = del.RightChild
+				}
+
+				root.Element = del.Element
+
+				root.LeftChild, newNode = Remove(root.LeftChild, del.Element)
+				root.LeftChild.Parent = root
+			} else {
+				del := root.RightChild
+				for !del.LeftChild.IsNil {
+					del = del.LeftChild
+				}
+
+				root.Element = del.Element
+
+				root.RightChild, newNode = Remove(root.RightChild, del.Element)
+				root.RightChild.Parent = root
+			}
+		} else {
+			if root.LeftChild.IsNil {
+				newNode = root.RightChild
+			} else if root.RightChild.IsNil {
+				newNode = root.LeftChild
+			}
+
+			// Recoloring of node for rbt deletion
+			//
+			if root.color == Black &&
+				(newNode.IsNil || newNode.color == Black) {
+				newNode.color = DoubleBlack
+			}
+			return newNode, newNode
 		}
-
-		// Case with 2 children
-		//
-		del := root.RightChild
-		for del.LeftChild != nil {
-			del = del.LeftChild
-		}
-
-		root.Element = del.Element
-
-		root.RightChild = Remove(root.RightChild, del.Element)
 	}
 
-	return root
+	return root, newNode
 }
 
 // LeftRotate rotates a node in a binary search tree
 func LeftRotate(x *Node) *Node {
-	if x == nil {
-		return nil
+	if x.IsNil {
+		return x
 	}
 
+	xRight := x.RightChild
 	// Get right Child
 	//
-	if x.RightChild == nil {
-		return nil
+	if xRight.IsNil {
+		return xRight
 	}
-	xRight := x.RightChild
 
 	// Assign parent of x as parent of right child
 	//
 	xParent := x.Parent
 	xRight.Parent = xParent
-	if xParent != nil {
+	if !xParent.IsNil {
 		if xParent.LeftChild == x {
 			xParent.LeftChild = xRight
 		} else if xParent.RightChild == x {
@@ -163,10 +211,10 @@ func LeftRotate(x *Node) *Node {
 	x.Parent = xRight
 	xRight.LeftChild = x
 
-	// Set left child of xright as right child of x
+	// Set right child of x to the left child of xright
 	//
 	x.RightChild = xRightLeft
-	if xRightLeft != nil {
+	if !xRightLeft.IsNil {
 		xRightLeft.Parent = x
 	}
 
@@ -175,22 +223,22 @@ func LeftRotate(x *Node) *Node {
 
 // RightRotate rotates a node in a binary search tree
 func RightRotate(x *Node) *Node {
-	if x == nil {
-		return nil
+	if x.IsNil {
+		return x
 	}
 
 	// Get left Child
 	//
-	if x.LeftChild == nil {
-		return nil
-	}
 	xLeft := x.LeftChild
+	if xLeft.IsNil {
+		return xLeft
+	}
 
-	// Assign parent of x as parent of right child
+	// Assign parent of x as parent of left child
 	//
 	xParent := x.Parent
 	xLeft.Parent = xParent
-	if xParent != nil {
+	if !xParent.IsNil {
 		if xParent.LeftChild == x {
 			xParent.LeftChild = xLeft
 		} else if xParent.RightChild == x {
@@ -200,15 +248,15 @@ func RightRotate(x *Node) *Node {
 
 	xLeftRight := xLeft.RightChild
 
-	// Set xright as the parent of x
+	// Set xleft as the parent of x
 	//
 	x.Parent = xLeft
 	xLeft.RightChild = x
 
-	// Set left child of xright as right child of x
+	// Set left child of x to right child of xleft
 	//
 	x.LeftChild = xLeftRight
-	if xLeftRight != nil {
+	if !xLeftRight.IsNil {
 		xLeftRight.Parent = x
 	}
 
--- binarysearchtree/redblacktree.go
+++ binarysearchtree/redblacktree.go
@@ -2,13 +2,15 @@ package binarysearchtree
 
 import (
 	"fmt"
+	"sync"
 )
 
 // Colors of a node of a red black tree
 const (
-	Uncolored = 0
-	Black     = 1
-	Red       = 2
+	Uncolored   = 0
+	Black       = 1
+	Red         = 2
+	DoubleBlack = 3
 )
 
 // Cases for binary search tree insertion
@@ -21,25 +23,48 @@ const (
 // red black property
 type RedBlackTree struct {
 	root *Node
+	sync.Mutex
+}
+
+// NewRedBlackTree creates a new red black tree instance
+func NewRedBlackTree() *RedBlackTree {
+	newRBT := &RedBlackTree{}
+	newRBT.root = NewNilNode()
+	return newRBT
 }
 
 // Insert adds an element to the red black tree
 func (r *RedBlackTree) Insert(e Element) {
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+
 	var newNode *Node
 	r.root, newNode = Insert(r.root, e)
 
-	r.fixViolation(newNode)
+	r.fixInsertViolation(newNode)
 }
 
-func (r *RedBlackTree) fixViolation(node *Node) {
-	if node == nil {
+// Remove deletes an element in the red black tree
+func (r *RedBlackTree) Remove(e Element) {
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+
+	var brokenNode *Node
+	r.root, brokenNode = Remove(r.root, e)
+
+	fmt.Println(brokenNode)
+	r.fixRemoveViolation(brokenNode)
+}
+
+func (r *RedBlackTree) fixInsertViolation(node *Node) {
+	if node.IsNil {
 		return
 	}
 	parent := node.Parent
 
 	// If root node, color it black
 	//
-	if parent == nil {
+	if parent.IsNil {
 		node.color = Black
 		r.root = node
 		return
@@ -61,7 +86,7 @@ func (r *RedBlackTree) fixViolation(node *Node) {
 
 	// This should never happen by logic
 	//
-	if grandParent == nil {
+	if grandParent.IsNil {
 		return
 	}
 
@@ -74,7 +99,7 @@ func (r *RedBlackTree) fixViolation(node *Node) {
 		insertCase = Left
 	}
 
-	if uncle == nil || uncle.color == Black {
+	if uncle.IsNil || uncle.color == Black {
 		if insertCase == Left {
 			// Case 1: Left right case
 			//
@@ -103,19 +128,115 @@ func (r *RedBlackTree) fixViolation(node *Node) {
 		parent.color = Black
 		grandParent.color = Red
 	}
-	r.fixViolation(grandParent)
+	r.fixInsertViolation(grandParent)
 }
 
-func (r RedBlackTree) String() string {
-	if r.root == nil {
-		return ""
+func (r *RedBlackTree) fixRemoveViolation(node *Node) {
+	if node.color == Black {
+		return
+	}
+	// Simple case broken node is colored red
+	//
+	if node.color == Red {
+		node.color = Black
+		return
+	}
+
+	// Get sibling
+	parent := node.Parent
+	// This should never happen
+	//
+	if parent.IsNil {
+		return
+	}
+	sibling := parent.LeftChild
+	removeCase := Left
+	if node == sibling {
+		removeCase = Right
+		sibling = parent.RightChild
+	}
+	// This should never happen again
+	//
+	fmt.Println("Node:", node, "\t\tSibling:", sibling, "\t\tparent:", parent)
+	if sibling.IsNil {
+		return
+	}
+
+	if sibling.color == Black {
+		// Cases when sibling is black
+		//
+		if (sibling.LeftChild.IsNil || sibling.LeftChild.color == Black) &&
+			(sibling.RightChild.IsNil || sibling.RightChild.color == Black) {
+			fmt.Println("Both black case")
+			// Case when both nephews are black
+			//
+			sibling.color = Red
+			if parent.color == Black {
+				parent.color = DoubleBlack
+			}
+		} else if removeCase == Left {
+			if sibling.LeftChild.IsNil || sibling.LeftChild.color == Black {
+				fmt.Println("Left right case")
+				// Left Right Case
+				//
+				sibling.RightChild.color, sibling.color = sibling.color, sibling.RightChild.color
+				sibling = LeftRotate(sibling)
+			}
+
+			fmt.Println("right right case")
+			// Right right case
+			//
+			parent.color, sibling.color = sibling.color, parent.color
+			parent = RightRotate(parent)
+			parent.LeftChild.color = Black
+		} else if removeCase == Right {
+			if sibling.LeftChild.IsNil || sibling.LeftChild.color == Black {
+				fmt.Println("right left case")
+				// Right Left Case
+				//
+				sibling.LeftChild.color, sibling.color = sibling.color, sibling.LeftChild.color
+				sibling = RightRotate(sibling)
+			}
+
+			fmt.Println("right right case")
+			// Right right case
+			//
+			parent.color, sibling.color = sibling.color, parent.color
+			parent = LeftRotate(parent)
+			parent.RightChild.color = Black
+		}
+		r.fixRemoveViolation(parent)
+	} else if sibling.color == Red {
+		// Cases when sibling is red
+		//
+		parent.color, sibling.color = sibling.color, parent.color
+		if removeCase == Left {
+			fmt.Println("left case")
+			// Left case
+			//
+			parent = RightRotate(parent)
+		} else if removeCase == Right {
+			fmt.Println("right case")
+			// Right case
+			//
+			parent = LeftRotate(parent)
+		}
+		r.fixRemoveViolation(node)
+	}
+	node.color = Black
+	return
+}
+
+func (r *RedBlackTree) String() string {
+	if r.root.IsNil {
+		return "<nil>"
 	}
 
 	return r.print2d(r.root, 0)
 }
 
-func (r RedBlackTree) print2d(node *Node, spaces int) string {
-	if node == nil {
+func (r *RedBlackTree) print2d(node *Node, spaces int) string {
+	if node.IsNil {
 		return ""
 	}
 
@@ -129,6 +250,8 @@ func (r RedBlackTree) print2d(node *Node, spaces int) string {
 	color := "r"
 	if node.color == Black {
 		color = "b"
+	} else if node.color == DoubleBlack {
+		color = "bb"
 	}
 	s += fmt.Sprintf("%d%s\n", node.Value(), color)
 
